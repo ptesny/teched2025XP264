779f69b093a2b4210632739624a6eaf9f02eeebd
ptesny <rth@tiosouji.com>
2025-10-19 15:53:29 +0100

    Update main.tf

--- exercises/ex1/k8s-mt/modules/btp-mt/main.tf
+++ exercises/ex1/k8s-mt/modules/btp-mt/main.tf
@@ -217,7 +217,7 @@ resource "btp_subaccount_subscription" "faas_xp264_mt" {
   }  
 }
 
-
+/*
 data "btp_subaccount_subscription" "faas_xp264_mt" {
   count = length(var.TECHED_MT_SUBSCRIPTION[*]) != 0 ? 1 : 0
 
@@ -225,9 +225,10 @@ data "btp_subaccount_subscription" "faas_xp264_mt" {
 
   subaccount_id = data.btp_subaccount.context.id
   app_name      = one(time_sleep.subscription_propagation[*].triggers["app_name"]) 
-  plan_name     = "default" //one(time_sleep.subscription_propagation[*].triggers["plan_name"]) 
+  plan_name     = "" //one(time_sleep.subscription_propagation[*].triggers["plan_name"]) 
 }
+*/
 
 output "faas_xp264_mt_subscription_url" {
-  value = one(data.btp_subaccount_subscription.faas_xp264_mt[*].subscription_url)
+  value = one(btp_subaccount_subscription.faas_xp264_mt[*].subscription_url)
 }
