8fadf0b9336a31a8d0a8b327e85c8045c3e63472
ptesny <rth@tiosouji.com>
2025-10-19 11:43:57 +0100

    Update bootstrap.tf

--- exercises/ex1/k8s-mt/bootstrap.tf
+++ exercises/ex1/k8s-mt/bootstrap.tf
@@ -3,6 +3,7 @@ module "mt_context" {
   source         = "./modules/btp-mt"
 
   BTP_SUBACCOUNT = var.BTP_SUBACCOUNT
+  BTP_CUSTOM_IDP - var.BTP_CUSTOM_IDP
   BTP_SA_REGION  = var.BTP_SA_REGION
   admin_groups   = var.admin_groups
 }
